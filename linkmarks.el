11bc36f68b921975f02358ec115131baf329f0b5
Karsten Klöss <karsten@kloess.xyz>
2023-11-26 12:36:42 +0100

    remove sideeffects of linkmarks--setup
    I did not like to see my org-settings getting changed

--- linkmarks.el
+++ linkmarks.el
@@ -28,39 +28,40 @@
   :group 'linkmarks
   :type 'string)
 
-(cl-defun linkmarks--setup ()
-  (setq org-outline-path-complete-in-steps nil)
-  (setq org-link-frame-setup '((elisp . find-file)
-                               (vm . vm-visit-folder-other-frame)
-                               (vm-imap . vm-visit-imap-folder-other-frame)
-                               (gnus . org-gnus-no-new-news)
-                               (file . find-file)
-                               (wl . wl-other-frame)))
-  (setq org-refile-use-outline-path t)
-  (setq org-refile-targets `((,linkmarks-file :maxlevel . 99)))
-  (setq org-confirm-elisp-link-function nil)
-  (setq safe-local-variable-values '((org-confirm-elisp-link-function . nil))))
+(cl-defmacro linkmarks--setup (&rest body)
+  `(let ((org-outline-path-complete-in-steps nil)
+         (org-link-frame-setup '((elisp . find-file)
+                                 (vm . vm-visit-folder-other-frame)
+                                 (vm-imap . vm-visit-imap-folder-other-frame)
+                                 (gnus . org-gnus-no-new-news)
+                                 (file . find-file)
+                                 (wl . wl-other-frame)))
+         (org-refile-use-outline-path t)
+         (org-refile-targets `((,linkmarks-file :maxlevel . 99)))
+         (org-confirm-elisp-link-function nil)
+         (safe-local-variable-values '((org-confirm-elisp-link-function . nil))))
+     ,@body))
 
 (cl-defun linkmarks--in-file ()
-  (linkmarks--setup)
-  (with-temp-buffer
-    (insert-file-contents linkmarks-file t)
-    (org-mode)
-    (goto-char (point-min))
-    (outline-show-all)
-    (cl-loop
-     for target in (org-refile-get-targets)
-     for element = (progn
-                     (goto-char (nth 3 target))
-                     (forward-line)
-                     (org-element-context))
-     for type = (car element)
-     for props = (cadr element)
-     for begin = (plist-get props :begin)
-     for end = (plist-get props :end)
-     for content = (buffer-substring begin end)
-     if (equal 'link type)
-     collect (list (car target) content))))
+  (linkmarks--setup
+   (with-temp-buffer
+     (insert-file-contents linkmarks-file t)
+     (org-mode)
+     (goto-char (point-min))
+     (outline-show-all)
+     (cl-loop
+      for target in (org-refile-get-targets)
+      for element = (progn
+                      (goto-char (nth 3 target))
+                      (forward-line)
+                      (org-element-context))
+      for type = (car element)
+      for props = (cadr element)
+      for begin = (plist-get props :begin)
+      for end = (plist-get props :end)
+      for content = (buffer-substring begin end)
+      if (equal 'link type)
+      collect (list (car target) content)))))
 
 ;;;###autoload
 (cl-defun linkmarks-select ()
@@ -75,9 +76,9 @@
 (defun linkmarks-capture ()
   (interactive)
   (let ((org-capture-templates '(("t" "Bookmark" entry (file linkmarks-file)
-                             "* %^{Title}\n[[%?]]\n  added: %U" :kill-buffer t))))
-    (linkmarks--setup)
-    (org-capture)))
+                                  "* %^{Title}\n[[%?]]\n  added: %U" :kill-buffer t))))
+    (linkmarks--setup
+     (org-capture))))
 
 (provide 'linkmarks)
 
